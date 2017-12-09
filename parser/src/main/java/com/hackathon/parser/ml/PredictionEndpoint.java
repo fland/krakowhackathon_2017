d2b6c949871822bdaeff8fc47f55af16e1cf858e
Konrad Cala <khcala@gmail.com>
2017-12-09 14:39:37 +0100

    PredictionEndpoint

---
+++ parser/src/main/java/com/hackathon/parser/ml/PredictionEndpoint.java
@@ -0,0 +1,24 @@
+package com.hackathon.parser.ml;
+
+import com.amazonaws.services.machinelearning.AmazonMachineLearning;
+import com.amazonaws.services.machinelearning.AmazonMachineLearningClientBuilder;
+import com.amazonaws.services.machinelearning.model.PredictRequest;
+import com.amazonaws.services.machinelearning.model.PredictResult;
+
+import java.util.Map;
+
+public class PredictionEndpoint {
+    PredictResult send(Map<String, String> dataRecord) {
+        AmazonMachineLearning amazonMachineLearning = AmazonMachineLearningClientBuilder.defaultClient();
+        PredictRequest predictRequest = createPredictionRequest(dataRecord);
+        return amazonMachineLearning.predict(predictRequest);
+    }
+
+    private PredictRequest createPredictionRequest(Map<String, String> dataRecord) {
+        PredictRequest predictRequest = new PredictRequest();
+        predictRequest.setMLModelId("ml-R6SJtWjzD4B");
+        predictRequest.setRecord(dataRecord);
+        predictRequest.setPredictEndpoint("https://realtime.machinelearning.eu-west-1.amazonaws.com");
+        return predictRequest;
+    }
+}
